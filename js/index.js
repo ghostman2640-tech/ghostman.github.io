95198008a7dca6c63e1757af0004fdfbe31ba268
ghostman2640-tech <ghostman2640@gmail.com>
2026-07-14 04:50:05 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
+const output = document.getElementById("output");
+const clearBtn = document.getElementById("clear-btn");
+const removeLastBtn = document.getElementById("remove-last");
+const nums = document.querySelectorAll(".nums");
+const assignments = document.querySelectorAll(".assignments");
 
+
+nums.forEach(num => {
+  num.addEventListener("click", event => {
+    output.innerHTML += event.target.innerHTML;
+  });
+});
+
+assignments.forEach(assignment => {
+  assignment.addEventListener("click", event => {
+    output.innerHTML += event.target.innerHTML;
+  });
+});
+
+clearBtn.addEventListener("click", () => {
+  output.innerHTML = "";
+});
+
+removeLastBtn.addEventListener("click", () => {
+  output.innerHTML = output.innerHTML.substr(0, output.innerHTML.length - 1);
+});
